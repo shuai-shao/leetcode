ef91a5c2d116da4a4e851d402c0e2bd40f44db78
Sean <sshaoking@gmail.com>
2015-01-01 15:29:06 -0500

    Create majority_element.py
    Another O(nlogn) method: sort and sorted(num)[len(num)/2] is always the desired number.

---
+++ majority_element.py
@@ -0,0 +1,21 @@
+class Solution:
+    # @param num, a list of integers
+    # @return an integer
+    def majorityElement(self, num):
+        length = len(num)
+        i = 0
+        found = False
+        while found == False:
+            target_num = num[i]
+            count = 0
+            for j in range(length):
+                if num[j] == target_num:
+                    count = count + 1
+            if count > length//2:
+                found = True
+                integer = target_num
+            else:
+                i = i + 1
+                while num[i] == num[i-1]:
+                    i = i + 1
+        return integer
